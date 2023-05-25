081b1a99b0e235559d81090c80ce871bc61d4434
Zachery Olson <Zachery.Olson@FamilySearch.org>
2023-05-25 14:55:36 -0600

    Work on Providers section, particularly FileProvider

---
+++ lib/config/src/new_parser/providers.rs
@@ -0,0 +1,105 @@
+#![allow(dead_code)]
+
+use super::OrTemplated;
+use serde::Deserialize;
+
+mod file;
+
+enum ProviderType {
+    File(file::FileProvider),
+    Response {
+        auto_return: ProviderSend,
+        buffer: BufferLimit,
+        unique: bool,
+    },
+    List(ListProvider),
+    Range {
+        start: i64,
+        end: i64,
+        step: u16,
+        repeat: bool,
+        unique: bool,
+    },
+}
+
+#[derive(Debug, Deserialize, PartialEq, Eq, Clone, Copy)]
+#[serde(rename_all = "snake_case")]
+enum ProviderSend {
+    Block,
+    Force,
+    IfNotFull,
+}
+
+/// Should hold nested json objects; just Strings for now
+struct ListProvider {
+    values: Vec<String>,
+    random: bool,
+    repeat: bool,
+    unique: bool,
+}
+
+enum ListProviderTmp {
+    JustAList(Vec<String>),
+    Defined {
+        values: Vec<String>,
+        random: bool,
+        repeat: bool,
+        unique: bool,
+    },
+}
+
+#[derive(Debug, Deserialize, PartialEq, Eq, Default, Clone, Copy)]
+#[serde(from = "BufferLimitTmp")]
+enum BufferLimit {
+    Limit(u64),
+    #[default]
+    Auto,
+}
+
+impl From<BufferLimitTmp> for BufferLimit {
+    fn from(value: BufferLimitTmp) -> Self {
+        match value {
+            BufferLimitTmp::Limit(x) => Self::Limit(x),
+            BufferLimitTmp::Auto(Auto::Auto) => Self::Auto,
+        }
+    }
+}
+
+/// Limit is supposed to be a number or the literal keyword "auto"
+/// This slightly redundant setup allows for that, but gets converted into the "real" limit struct
+/// after.
+#[derive(Debug, Deserialize, PartialEq, Eq)]
+#[serde(untagged)]
+enum BufferLimitTmp {
+    Limit(u64),
+    Auto(Auto),
+}
+
+#[derive(Debug, Deserialize, PartialEq, Eq)]
+#[serde(rename_all = "snake_case")]
+enum Auto {
+    Auto,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_yaml::from_str as from_yaml;
+
+    #[test]
+    fn test_basic_types() {
+        // provider send
+        let ps: ProviderSend = from_yaml("!block").unwrap();
+        assert_eq!(ps, ProviderSend::Block);
+        let ps: ProviderSend = from_yaml("!force").unwrap();
+        assert_eq!(ps, ProviderSend::Force);
+        let ps: ProviderSend = from_yaml("!if_not_full").unwrap();
+        assert_eq!(ps, ProviderSend::IfNotFull);
+
+        // buffer limit
+        let bl: BufferLimit = from_yaml("43").unwrap();
+        assert_eq!(bl, BufferLimit::Limit(43));
+        let bl: BufferLimit = from_yaml("auto").unwrap();
+        assert_eq!(bl, BufferLimit::Auto);
+    }
+}
