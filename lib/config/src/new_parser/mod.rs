4c6cd31380865b57dea0de3555e3d505ddfb1c51
Zachery Olson <Zachery.Olson@FamilySearch.org>
2023-05-30 09:15:48 -0600

    define LoadTest struct

--- lib/config/src/new_parser/mod.rs
+++ lib/config/src/new_parser/mod.rs
@@ -1,4 +1,9 @@
+#![allow(dead_code)]
+
 pub mod templating;
+use serde::Deserialize;
+use std::collections::BTreeMap;
+
 pub use templating::OrTemplated;
 
 pub mod config;
@@ -8,3 +13,13 @@ pub mod loggers;
 pub mod providers;
 
 pub mod common;
+
+#[derive(Debug, Deserialize)]
+pub struct LoadTest {
+    config: config::Config,
+    load_pattern: load_pattern::LoadPattern,
+    vars: templating::Vars,
+    providers: BTreeMap<String, providers::ProviderType>,
+    loggers: BTreeMap<String, loggers::Logger>,
+    endpoints: BTreeMap<String, endpoints::Endpoint>,
+}
