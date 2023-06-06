a138431fda0af007e4bc1407b40dcb64bbedc35b
Zachery Olson <Zachery.Olson@FamilySearch.org>
2023-06-06 09:13:00 -0600

    Finish Vars insertion, and add comments/tests

--- lib/config/src/configv2/templating.rs
+++ lib/config/src/configv2/templating.rs
@@ -1,88 +1,28 @@
+//! Templating-related types. The generic type vars `VD` and `ED` correspond to "Vars Done", meaning
+//! that static vars have been inserted, and "Envs Done", meaning that OS Environment variables have been
+//! inserted.
+//!
+//! Rather than redefine nearly identical types multiple times for the same structure before and
+//! after var processing, this module uses conditional enums to manage state, as well as which
+//! template sources are allowed.
+//!
+//! Read here for more info: <https://rreverser.com/conditional-enum-variants-in-rust/>
+//!
+//! For example: `Template<_, EnvsOnly>` cannot be instantiated in the PreVars variant, because the
+//! associated type is False.
+
+use super::PropagateVars;
+use derivative::Derivative;
+pub use helpers::*;
 use itertools::Itertools;
 use once_cell::sync::Lazy;
 use regex::Regex;
 use serde::Deserialize;
-use std::{collections::BTreeMap, convert::TryFrom, fmt, str::FromStr};
+use std::{collections::BTreeMap, convert::TryFrom, str::FromStr};
 use thiserror::Error;
 
-use super::PropagateVars;
-
-#[derive(Default, Deserialize, Debug, PartialEq, Eq, Clone, Copy)]
-pub struct True;
-
-#[derive(Deserialize, Debug, PartialEq, Eq, Clone, Copy)]
-pub enum False {}
-
-pub trait TryDefault: Sized + fmt::Debug {
-    fn try_default() -> Result<Self, &'static str>;
-}
-
-impl TryDefault for True {
-    fn try_default() -> Result<Self, &'static str> {
-        Ok(Self)
-    }
-}
-
-impl TryDefault for False {
-    fn try_default() -> Result<Self, &'static str> {
-        Err("uninhabited type")
-    }
-}
-
-pub trait Bool: fmt::Debug + TryDefault + Clone + Copy + PartialEq + Eq {
-    type Inverse: Bool + fmt::Debug;
-
-    const VALUE: bool;
-}
-
-pub trait OK: Default {}
-
-impl OK for True {}
-
-impl Bool for True {
-    type Inverse = False;
-    const VALUE: bool = true;
-}
-
-impl Bool for False {
-    type Inverse = True;
-    const VALUE: bool = false;
-}
-
-pub trait TemplateType: fmt::Debug {
-    type EnvsAllowed: Bool;
-    type VarsAllowed: Bool;
-    type ProvAllowed: Bool;
-}
-
-#[derive(Deserialize, Debug)]
-pub struct EnvsOnly;
-
-impl TemplateType for EnvsOnly {
-    type EnvsAllowed = True;
-    type VarsAllowed = False;
-    type ProvAllowed = False;
-}
-
-#[derive(Deserialize, Debug, PartialEq, Eq, Clone, Copy)]
-pub struct VarsOnly;
-
-impl TemplateType for VarsOnly {
-    type EnvsAllowed = False;
-    type VarsAllowed = True;
-    type ProvAllowed = False;
-}
-
-#[derive(Deserialize, Debug, PartialEq, Eq, Clone, Copy)]
-pub struct Regular;
-
-impl TemplateType for Regular {
-    type EnvsAllowed = False;
-    type VarsAllowed = True;
-    type ProvAllowed = True;
-}
-
-#[derive(Deserialize, Debug, PartialEq, Eq, Clone)]
+#[derive(Deserialize, PartialEq, Eq, Clone, Derivative)]
+#[derivative(Debug)]
 #[serde(try_from = "TemplateTmp<V, T>")]
 pub enum Template<
     V: FromStr,
@@ -95,15 +35,18 @@ pub enum Template<
     },
     Env {
         template: TemplatedString<T>,
+        #[derivative(Debug = "ignore")]
         __dontuse: (T::EnvsAllowed, ED::Inverse),
     },
     PreVars {
         template: TemplatedString<T>,
+        #[derivative(Debug = "ignore")]
         __dontuse: (T::VarsAllowed, VD::Inverse),
     },
     // needs more work done on this
     NeedsProviders {
         script: TemplatedString<T>,
+        #[derivative(Debug = "ignore")]
         __dontuse: (ED, VD, T::ProvAllowed),
     },
 }
@@ -306,6 +249,8 @@ enum TemplatePiece<T: TemplateType> {
     Provider(String, T::ProvAllowed),
 }
 
+/// Temporary Template type that allows all variants to exist, but can fail in conversion into the
+/// main Template based on the marker type of `T`.
 #[derive(Debug, Deserialize, Clone)]
 enum TemplateTmp<V, T: TemplateType> {
     #[serde(rename = "l")]
@@ -325,26 +270,7 @@ enum TemplateError {
     #[error(r#"invalid template type "{0}" with value "{1}""#)]
     InvalidTemplateForType(&'static str, String),
 }
-/*
-fn validate_template_types<T: TemplateType>(
-    t: TemplatedString,
-) -> Result<TemplatedString, TemplateError> {
-    let error =
-        t.0.iter().find_map(|ts| match ts {
-            TemplatePiece::Raw(_) => None,
-            TemplatePiece::Env(e) => (!T::EnvsAllowed::VALUE)
-                .then(|| TemplateError::InvalidTemplateForType("e", e.clone())),
-            TemplatePiece::Var(v) => (!T::VarsAllowed::VALUE)
-                .then(|| TemplateError::InvalidTemplateForType("v", v.clone())),
-            TemplatePiece::Provider(p) => (!T::ProvAllowed::VALUE)
-                .then(|| TemplateError::InvalidTemplateForType("p", p.clone())),
-        });
-    match error {
-        Some(e) => Err(e),
-        None => Ok(t),
-    }
-}
-*/
+
 impl<V: FromStr, T: TemplateType, ED: Bool, VD: Bool> TryFrom<TemplateTmp<V, T>>
     for Template<V, T, ED, VD>
 {
@@ -389,24 +315,193 @@ where
     }
 }
 
-/*
-#[test]
-fn test_new_templates() {
-    serde_yaml::from_str::<Template<i32, EnvsOnly, True, True>>("!l 5").unwrap();
-    serde_yaml::from_str::<Template<i32, VarsOnly, False, False>>("!l 5").unwrap();
-    serde_yaml::from_str::<Template<i32, EnvsOnly>>("!e ${e:HOME}").unwrap();
-    serde_yaml::from_str::<Template<i32, VarsOnly>>("!e ${e:HOME}").unwrap_err();
-    serde_yaml::from_str::<Template<i32, VarsOnly>>("!v ${v:x}").unwrap();
-    serde_yaml::from_str::<Template<i32, Regular>>("!v ${v:x}").unwrap();
-    serde_yaml::from_str::<Template<i32, Regular>>("!v ${e:HOME}").unwrap_err();
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    let t = "hello".parse::<TemplatedString>().unwrap();
-    assert_eq!(t.0, vec![TemplatePiece::Raw("hello".to_owned())]);
+    #[test]
+    fn templated_strings_raw() {
+        let TemplatedString::<EnvsOnly>(t) = "hello".parse().unwrap();
+        assert_eq!(t, vec![TemplatePiece::Raw("hello".to_owned())]);
+        let TemplatedString::<VarsOnly>(t) = "hello".parse().unwrap();
+        assert_eq!(t, vec![TemplatePiece::Raw("hello".to_owned())]);
+        let TemplatedString::<Regular>(t) = "hello".parse().unwrap();
+        assert_eq!(t, vec![TemplatePiece::Raw("hello".to_owned())]);
+    }
 
-    let TemplatedString(v) = "${e:HOME}".parse().unwrap();
-    assert_eq!(v, vec![TemplatePiece::Env("HOME".to_owned())]);
-    let TemplatedString(v) = "${v:x}".parse().unwrap();
-    assert_eq!(v, vec![TemplatePiece::Var("x".to_owned())]);
-    let TemplatedString(v) = "${p:foobar}".parse().unwrap();
-    assert_eq!(v, vec![TemplatePiece::Provider("foobar".to_owned())]);
-}*/
+    #[test]
+    fn templated_strings_single() {
+        let TemplatedString::<EnvsOnly>(v) = "${e:HOME}".parse().unwrap();
+        assert_eq!(
+            v,
+            vec![TemplatePiece::Env(
+                "HOME".to_owned(),
+                TryDefault::try_default().unwrap()
+            )]
+        );
+        let TemplatedString::<VarsOnly>(v) = "${v:x}".parse().unwrap();
+        assert_eq!(
+            v,
+            vec![TemplatePiece::Var(
+                "x".to_owned(),
+                TryDefault::try_default().unwrap()
+            )]
+        );
+        let TemplatedString::<Regular>(v) = "${v:x}".parse().unwrap();
+        assert_eq!(
+            v,
+            vec![TemplatePiece::Var(
+                "x".to_owned(),
+                TryDefault::try_default().unwrap()
+            )]
+        );
+        let TemplatedString::<Regular>(v) = "${p:foobar}".parse().unwrap();
+        assert_eq!(
+            v,
+            vec![TemplatePiece::Provider(
+                "foobar".to_owned(),
+                TryDefault::try_default().unwrap()
+            )]
+        );
+    }
+
+    #[test]
+    fn template_strings_vec() {
+        assert_eq!(
+            "foo${v:bar}".parse::<TemplatedString<VarsOnly>>(),
+            Ok(TemplatedString(vec![
+                TemplatePiece::Raw("foo".to_owned()),
+                TemplatePiece::Var("bar".to_owned(), TryDefault::try_default().unwrap())
+            ]))
+        );
+
+        assert_eq!(
+            "${e:HOME}/file.txt".parse::<TemplatedString<EnvsOnly>>(),
+            Ok(TemplatedString(vec![
+                TemplatePiece::Env("HOME".to_owned(), TryDefault::try_default().unwrap()),
+                TemplatePiece::Raw("file.txt".to_owned()),
+            ]))
+        );
+    }
+}
+
+mod helpers {
+    use serde::Deserialize;
+    use std::fmt;
+
+    mod private {
+        pub trait Seal {}
+
+        impl Seal for super::True {}
+        impl Seal for super::False {}
+        impl Seal for super::EnvsOnly {}
+        impl Seal for super::VarsOnly {}
+        impl Seal for super::Regular {}
+        impl<T, U> Seal for (T, U)
+        where
+            T: Seal,
+            U: Seal,
+        {
+        }
+        impl<T, U, V> Seal for (T, U, V)
+        where
+            T: Seal,
+            U: Seal,
+            V: Seal,
+        {
+        }
+    }
+
+    /// Unit type that only exists to allow enum variants containing to be made.
+    #[derive(Default, Deserialize, Debug, PartialEq, Eq, Clone, Copy)]
+    pub struct True;
+
+    /// Uninhabited type that makes enum variants containing it to be inaccessible.
+    #[derive(Deserialize, Debug, PartialEq, Eq, Clone, Copy)]
+    pub enum False {}
+
+    /// Trait for trying to get a Default value. Serde itself has no solution (that I could find)
+    /// that directly allows making specific enum variants inaccessible, so this is to make
+    /// generating a Default value fallible based on the type. If an invaild variant is used (for
+    /// example, an env variant for a template outside of the vars section), then
+    /// `False::try_default()` will be called, and an error will be forwarded and Deserialize will
+    /// fail.
+    pub trait TryDefault: Sized + fmt::Debug + private::Seal {
+        fn try_default() -> Result<Self, &'static str>;
+    }
+
+    impl TryDefault for True {
+        fn try_default() -> Result<Self, &'static str> {
+            Ok(Self)
+        }
+    }
+
+    impl TryDefault for False {
+        fn try_default() -> Result<Self, &'static str> {
+            Err("uninhabited type")
+        }
+    }
+
+    /// Trait for a type that represents a boolean state for if a value can be constructed.
+    pub trait Bool:
+        fmt::Debug + TryDefault + Clone + Copy + PartialEq + Eq + private::Seal
+    {
+        type Inverse: Bool + fmt::Debug;
+
+        const VALUE: bool;
+    }
+
+    /// Trait meaning that the Boolean type specifically can be created.
+    pub trait OK: Default + Bool + private::Seal {}
+
+    impl OK for True {}
+
+    impl Bool for True {
+        type Inverse = False;
+        const VALUE: bool = true;
+    }
+
+    impl Bool for False {
+        type Inverse = True;
+        const VALUE: bool = false;
+    }
+
+    /// Trait for types of templatings allowed. It's not an enumeration of variants, because
+    /// Template needs to be generic over a type of this trait.
+    pub trait TemplateType: fmt::Debug + private::Seal + PartialEq + Eq {
+        type EnvsAllowed: Bool;
+        type VarsAllowed: Bool;
+        type ProvAllowed: Bool;
+    }
+
+    /// Marker struct to indicate that this template can only read from OS environment variables as
+    /// a source.
+    #[derive(Deserialize, Debug, PartialEq, Eq, Clone, Copy)]
+    pub struct EnvsOnly;
+
+    impl TemplateType for EnvsOnly {
+        type EnvsAllowed = True;
+        type VarsAllowed = False;
+        type ProvAllowed = False;
+    }
+
+    /// Marker struct to indicate that this template can only read from static Vars as a source.
+    #[derive(Deserialize, Debug, PartialEq, Eq, Clone, Copy)]
+    pub struct VarsOnly;
+
+    impl TemplateType for VarsOnly {
+        type EnvsAllowed = False;
+        type VarsAllowed = True;
+        type ProvAllowed = False;
+    }
+
+    /// Marker struct to indicate that this template can read from vars or providers.
+    #[derive(Deserialize, Debug, PartialEq, Eq, Clone, Copy)]
+    pub struct Regular;
+
+    impl TemplateType for Regular {
+        type EnvsAllowed = False;
+        type VarsAllowed = True;
+        type ProvAllowed = True;
+    }
+}
